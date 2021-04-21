4571c0261bab7f88d5f6a6ea16ab1f94b9815cc4
santiracca <santiagoracca7@gmail.com>
2021-04-21 16:18:30 -0400

    tween animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:animations/screens/home.dart';
-import 'package:animations/screens/sandbox.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ninja Trips',
-      home: Sandbox(),
+      home: Home(),
     );
   }
 }
--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -7,9 +7,23 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: TextStyle(fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    return TweenAnimationBuilder(
+      duration: Duration(milliseconds: 500),
+      builder: (BuildContext context, double val, child) {
+        return Opacity(
+          opacity: val,
+          child: Padding(
+            child: child,
+            padding: EdgeInsets.only(top: val * 20),
+          ),
+        );
+      },
+      tween: Tween<double>(begin: 0, end: 1),
+      child: Text(
+        text,
+        style: TextStyle(
+            fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+      ),
     );
   }
 }
